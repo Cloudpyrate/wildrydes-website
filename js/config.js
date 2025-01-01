19d3a52c2c2539a3c69a6750f05a7d9f4989ed5e
Cloudpyrate <149804153+Cloudpyrate@users.noreply.github.com>
2025-01-01 23:18:56 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_gkmODiZ7t', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1j2uljctap180eo6nk9r9pssri', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
